a6861cf950bcdec158f4fc10a22f09cd56a7d3fb
João Santana <joaosantana2001@icloud.com>
2019-07-01 10:47:14 -0300

    Created Curso-Detalhe component for Cursos Module, integrating it with components of another module.

--- primeiro-projeto/src/app/cursos/cursos.module.ts
+++ primeiro-projeto/src/app/cursos/cursos.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CursosComponent } from './cursos.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { CursosComponent } from "./cursos.component";
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 @NgModule({
-  declarations: [CursosComponent],
-  imports: [
-    CommonModule
-  ]
+  declarations: [CursosComponent, CursoDetalheComponent],
+  imports: [CommonModule],
+  exports: [CursosComponent]
 })
-export class CursosModule { }
+export class CursosModule {}
